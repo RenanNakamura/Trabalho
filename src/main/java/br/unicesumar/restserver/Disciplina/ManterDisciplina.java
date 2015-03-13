1b32a470bbc8a1c5aaa66d761d9fc1ef17e7f4fd
nakamurarenan <nakamura_renan@hotmail.com>
2015-03-12 21:40:34 -0300

    #1 - Subindo o projeto base

--- src/main/java/br/unicesumar/restserver/Disciplina/ManterDisciplina.java
+++ src/main/java/br/unicesumar/restserver/Disciplina/ManterDisciplina.java
@@ -1,13 +1,42 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.unicesumar.restserver.Disciplina;
 
-/**
- *
- * @author Alunos
- */
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import javax.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Disciplinas")
+@Transactional
 public class ManterDisciplina {
+    @Autowired
+    private EntityManager em;
     
+    @RequestMapping(method = RequestMethod.GET )
+    public List<Disciplina> getDisciplina(){
+        return this.em.createQuery("from Disciplina").getResultList();
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public void criarDisciplina(@RequestBody Disciplina disciplina){
+        em.persist(disciplina);
+    }
+    
+    @RequestMapping(method = RequestMethod.PUT)
+    public void atualizaDisciplina(@RequestBody Disciplina disciplina){
+        disciplina = em.merge(disciplina);
+        this.em.persist(disciplina);
+    }
+    
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public void removeDisciplina(@PathVariable long id){
+        this.em.createQuery("delete from Disciplina d where d.id = :id").setParameter("id", id).executeUpdate();         
+        
+    }
 }
---
+++ src/main/webapp/pages/Disciplina/Disciplina.js
@@ -0,0 +1,72 @@
+module = angular.module("Disciplina", []);
+
+module.controller("ManterDisciplina", ["$scope","$http", ManterDisciplina]);
+
+
+function ManterDisciplina($scope,$http) {
+    
+    $scope.iniciar = funcaoIniciar;
+    $scope.salvar = funcaoSalvar;
+    $scope.excluir = funcaoExcluir;
+    $scope.editar = funcaoEditar;
+    
+    $scope.disciplinas = [];
+    $scope.disciplina = {};
+    $scope.isNovo = true;
+    
+    function funcaoEditar(aValor) {
+        $scope.disciplina = angular.copy(aValor);
+        $scope.isNovo = false;
+    }
+
+    
+    function funcaoExcluir(aValor) {
+        $scope.delete("/Disciplinas/" + aValor.id).success(onSuccess).error(onError);
+        
+        function onSuccess(data, status){
+            console.log("entrou!");
+            funcaoCarregar();
+        }
+        
+        function onError(data, status){
+            alert("Deu erro!" + data);
+        }
+     }
+    
+    function funcaoSalvar() {
+        if($scope.isNovo){
+            $http.post("/Disciplinas", $scope.disciplina).success(onSuccess).error(onError);
+        }else{
+            $http.put("/Disciplinas", $scope.disciplina).success(onSuccess).error(onError);
+        }
+        
+        function onSuccess(data, status){
+            funcaoCarregar();
+            $scope.disciplina = {};
+            $scope.isNovo = true;
+        }
+        
+        function onError(data, status){
+            alert("Deu erro!" + data);
+        }
+    }
+    
+    function funcaoCarregar() {
+        $http.get("/Disciplinas").success(onSuccess).error(onError);
+        
+        function onSuccess(data, status) {
+            $scope.disciplinas = data;       
+            console.log(data);
+        }
+        function onError(data, status) {
+            alert("Deu erro: " + data);
+        }
+    }
+    
+    function funcaoIniciar() {
+        funcaoCarregar();
+    }
+        
+}
+
+
